7c7ad16383eed2474dd9e7a43301d4d63ee290dc
AnmolPreet <anmolpreet@techhiedunia.com>
2020-12-20 17:55:50 +0530

    First Commit

---
+++ app/src/main/java/com/example/androidbanking/AllAccountOptions.java
@@ -0,0 +1,14 @@
+package com.example.androidbanking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllAccountOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_account_options);
+    }
+}
---
+++ app/src/main/java/com/example/androidbanking/DepostMoney.java
@@ -0,0 +1,14 @@
+package com.example.androidbanking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepostMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_depost_money);
+    }
+}
